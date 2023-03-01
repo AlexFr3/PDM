6b840e7d47a1491616abe2dba17e391e06e9ae7b
AlexFr3 <alexfrisoni2003@gmail.com>
2023-03-02 00:49:54 +0100

    Prova tabella, Prova casella di testo
    Resize, jquery, css

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-window.addEventListener("load", function() {
-  var tabella = document.querySelector("#tabella-spostabile");
-  var posizioneInizialeX;
-  var posizioneInizialeY;
-  var posizioneAttualeX;
-  var posizioneAttualeY;
-  var offsetX;
-  var offsetY;
-
-  var riquadri = document.querySelectorAll(".col-md-3");
-
-for (var i = 0; i < riquadri.length; i++) {
-  riquadri[i].addEventListener("drop", function(event) {
-    event.preventDefault();
-    this.appendChild(tabella);
-  });
-
-  riquadri[i].addEventListener("dragover", function(event) {
-    event.preventDefault();
-  });
-}
-
-tabella.addEventListener("dragstart", function(event) {
-  event.dataTransfer.setData("text", this.id);
-});
-  tabella.addEventListener("mousedown", function(event) {
-    posizioneInizialeX = event.clientX;
-    posizioneInizialeY = event.clientY;
-    offsetX = tabella.offsetLeft - posizioneInizialeX;
-    offsetY = tabella.offsetTop - posizioneInizialeY;
-
-    document.addEventListener("mouseup", function() {
-      document.removeEventListener("mousemove", spostaTabella);
-    });
-
-    document.addEventListener("mousemove", spostaTabella);
-  });
-
-  function spostaTabella(event) {
-    posizioneAttualeX = event.clientX;
-    posizioneAttualeY = event.clientY;
-    tabella.style.left = (posizioneAttualeX + offsetX) + "px";
-    tabella.style.top = (posizioneAttualeY + offsetY) + "px";
+function btnEdit(btnEdit) {
+  // istruzioni
   }
+$(function() {
+  // rendi l'elemento con classe "movable" spostabile
+  $('.movable').draggable({
+    // disabilita la selezione del testo durante il trascinamento
+    start: function(event, ui) {
+      $(this).find('input').attr('readonly', 'readonly');
+    },
+    stop: function(event, ui) {
+      $(this).find('input').removeAttr('readonly');
+    }
+  });
 });
